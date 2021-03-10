a4a822169ef9fc26a0f56d12f407445c10fb55c7
rleme <rodrigocleme@gmail.com>
2021-03-10 13:16:26 -0500

    Casa 012 phpinfo

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,11 @@
+
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
+
+// Mostra todas as informações, usa o padrão INFO_ALL
+phpinfo();
+
+// Mostra apenas informações dos módulos.
+// phpinfo(8) mostra um resultado identico.
+phpinfo(INFO_MODULES);
+
 ?>
